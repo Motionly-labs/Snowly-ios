df6c831206f96a56a548ec69d36f1a3864650ad2
Roy Kid <lijichen365@gmail.com>
2026-03-02 10:23:54 +0100

    Initial Commit

---
+++ Snowly/SnowlyApp.swift
@@ -0,0 +1,32 @@
+//
+//  SnowlyApp.swift
+//  Snowly
+//
+//  Created by Roy Kid on 2026-03-02.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SnowlyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SnowlyTests/SnowlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnowlyTests.swift
+//  SnowlyTests
+//
+//  Created by Roy Kid on 2026-03-02.
+//
+
+import Testing
+@testable import Snowly
+
+struct SnowlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
